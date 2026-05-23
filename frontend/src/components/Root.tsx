78f4119497fb1ababc805ed5074fa0034e317bd7
Montse Garcia <montsegarciaj@hotmail.com>
2026-05-23 20:40:07 +0200

    navbar

--- frontend/src/components/Root.tsx
+++ frontend/src/components/Root.tsx
@@ -1,13 +1,10 @@
-import AppHeader from "./AppHeader";
+import NavBar from "./NavBar";
 import { Outlet } from "react-router";
 
 const Root = () => {
     return (
         <div className="app">
-            <header className="app-header">
-                <h1 className="app-title">TDBO</h1>
-                <AppHeader />
-            </header>
+            <NavBar />
 
             <main className="app-main">
                 <Outlet />
