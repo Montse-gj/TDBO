9354243c61f9e5ecc449a8727bc8c4f97b567e81
r3dc0m <jonathanvelez85@gmail.com>
2026-05-25 11:16:47 +0200

    0525.1116 cambio en vite.config para que el proxy apunte al nombre del contenedor directamente -> para mejora futura, poner el host en el .env y que tire el proxy del mismo .env un nivel más arriba

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://tdbo-back:3000",
         changeOrigin: true,
       },
     },
