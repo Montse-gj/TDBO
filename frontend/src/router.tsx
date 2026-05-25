248fcb7d9e980a218ebd8276b33cc73acbf691e8
Montse Garcia <montsegarciaj@hotmail.com>
2026-05-25 12:51:18 +0200

    redirescciones

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,11 +4,12 @@ import Error from './pages/Error';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Page3 from './pages/Page3';
+import Trips from './pages/Trips';
 import Page4 from './pages/Page4';
 import Page5 from './pages/Page5';
 import User from './pages/User';
 import Dashboard from './pages/Dashboard';
+
 
 const router = createBrowserRouter([
     {
@@ -21,7 +22,7 @@ const router = createBrowserRouter([
             { path: 'login', element: <Login /> },
             { path: 'register', element: <Register /> },
             { path: 'dashboard', element: <Dashboard /> },
-            { path: 'page3', element: <Page3 /> },
+            { path: 'trips', element: <Trips /> },
             { path: 'page4', element: <Page4 /> },
             { path: 'page5', element: <Page5 /> },
             { path: 'user', element: <User /> },
