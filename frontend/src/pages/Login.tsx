15c8ab92f42d462bf2024ba46b3ce1ba5b11fbd4
lualvarimp <lualvarimp@gmail.com>
2026-05-26 13:29:52 +0200

    Responsive design de Register/Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -34,15 +34,15 @@ export const Login = () => {
 
     return (
         <>
-          
-            <div className="">
-                <div className="">
-                    <h1>Login</h1>
+
+            <div className="login-wrapper">
+                <div className="login-form">
+                    <h1><span>L</span><span>og</span><span>in</span></h1>
                     {displayError && <p>{displayError}</p>}
                     <form onSubmit={handleSubmit}>
                         <div className="">
                             <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" value={email} onChange={(e) => {
+                            <input type="email" id="email" placeholder="tuemail@ejemplo.com" value={email} onChange={(e) => {
                                 setEmail(e.target.value)
                                 setLocalError("")
                             }}
@@ -50,7 +50,7 @@ export const Login = () => {
                         </div>
                         <div className="">
                             <label htmlFor="password">Password:</label>
-                            <input type="password" id="password" value={password} onChange={(e) => {
+                            <input type="password" id="password" placeholder="la palabra que recuerdas" value={password} onChange={(e) => {
                                 setPassword(e.target.value)
                                 setLocalError("")
                             }}
@@ -60,8 +60,14 @@ export const Login = () => {
                         </div>
 
                         <button className="" type="submit" disabled={loading}>Login</button>
+                        <p>¿No tienes cuenta? <Link to="/register">Regístrate</Link></p>
                     </form>
-                    <p>¿No tienes cuenta? <Link to="/register">Regístrate</Link></p>
+
+                </div>
+                <div className="image-wrapper">
+                    <div>
+                        <img src="../src/assets/a-new-travel.webp" alt="Logging in to the TDBO website is very easy" />
+                    </div>
                 </div>
             </div>
         </>
