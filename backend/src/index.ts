1cba985ac8c66223e0b62c0ae50a70949ef4af5e
r3dc0m <jonathanvelez85@gmail.com>
2026-05-28 01:44:56 +0200

    0528.0144 fix de compose.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,15 +18,12 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
     swaggerOptions: {
         docExpansion: 'none'
     }
 }));
-
-app.get('/', (req, res) => {
-    res.json({ status: 'ok' });
-});
 
 async function startServer() {
     await checkDB();
